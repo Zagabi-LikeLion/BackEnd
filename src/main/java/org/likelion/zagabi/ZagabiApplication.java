eb5a03aac91382699346ac5837e44fd313a9fe8a
Geunsik Kim <geunsikevin@naver.com>
2024-07-21 23:38:50 +0900

    project initialization

---
+++ src/main/java/org/likelion/zagabi/ZagabiApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.zagabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagabiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZagabiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/likelion/zagabi/ZagabiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.zagabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagabiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
