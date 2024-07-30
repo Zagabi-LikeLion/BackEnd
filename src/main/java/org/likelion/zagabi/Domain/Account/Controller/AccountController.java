3e63a38568fb6ba6ea8f7904813994cee562fa22
Geunsik Kim <geunsikevin@naver.com>
2024-07-30 17:05:54 +0900

    ✨feat: 토큰 재발급 기능 구현(#26)

--- src/main/java/org/likelion/zagabi/Domain/Account/Controller/AccountController.java
+++ src/main/java/org/likelion/zagabi/Domain/Account/Controller/AccountController.java
@@ -1,11 +1,16 @@
 package org.likelion.zagabi.Domain.Account.Controller;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.likelion.zagabi.Domain.Account.Dto.Request.*;
 import org.likelion.zagabi.Domain.Account.Dto.Response.UserLoginResponseDto;
 import org.likelion.zagabi.Domain.Account.Dto.Response.UserSignUpResponseDto;
 import org.likelion.zagabi.Domain.Account.Entity.User;
+import org.likelion.zagabi.Domain.Account.Jwt.Dto.JwtDto;
+import org.likelion.zagabi.Domain.Account.Jwt.Exception.SecurityCustomException;
+import org.likelion.zagabi.Domain.Account.Jwt.Exception.TokenErrorCode;
+import org.likelion.zagabi.Domain.Account.Jwt.Util.JwtProvider;
 import org.likelion.zagabi.Domain.Account.Service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final JwtProvider jwtProvider;
 
     @PostMapping("/login")
     public ResponseEntity<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
@@ -56,5 +62,18 @@ public class AccountController {
     public ResponseEntity<Void> changeNickname(HttpServletRequest request, @RequestBody ChangeNicknameRequestDto requestDto) {
         accountService.changeNickname(request, requestDto);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<JwtDto> reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
+        try {
+            jwtProvider.validateRefreshToken(refreshToken);
+            JwtDto newToken = jwtProvider.reissueToken(refreshToken);
+            return ResponseEntity.ok(newToken);
+        } catch (ExpiredJwtException eje) {
+            throw new SecurityCustomException(TokenErrorCode.TOKEN_EXPIRED, eje);
+        } catch (IllegalArgumentException iae) {
+            throw new SecurityCustomException(TokenErrorCode.INVALID_TOKEN, iae);
+        }
     }
 }
