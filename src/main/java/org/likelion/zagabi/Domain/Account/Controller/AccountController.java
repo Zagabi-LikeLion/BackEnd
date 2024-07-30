8067a224d1207a3f65dfe48b3d187c998e188a37
Jimyeong Kim <kjm021221@icloud.com>
2024-07-30 17:04:30 +0900

    ♻️refactor: Account 부분 role 값 삭제 및 코드 리팩토링

--- src/main/java/org/likelion/zagabi/Domain/Account/Controller/AccountController.java
+++ src/main/java/org/likelion/zagabi/Domain/Account/Controller/AccountController.java
@@ -1,11 +1,11 @@
 package org.likelion.zagabi.Domain.Account.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.likelion.zagabi.Domain.Account.Dto.Request.*;
 import org.likelion.zagabi.Domain.Account.Dto.Response.UserLoginResponseDto;
 import org.likelion.zagabi.Domain.Account.Dto.Response.UserSignUpResponseDto;
-import org.likelion.zagabi.Domain.Account.Entity.User;
 import org.likelion.zagabi.Domain.Account.Service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +17,13 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/login")
-    public ResponseEntity<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
+    public ResponseEntity<UserLoginResponseDto> login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         UserLoginResponseDto responseDto = accountService.login(requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<UserSignUpResponseDto> signup(@RequestBody UserSignUpRequestDto requestDto) {
+    public ResponseEntity<UserSignUpResponseDto> signup(@RequestBody @Valid UserSignUpRequestDto requestDto) {
         UserSignUpResponseDto responseDto = accountService.signup(requestDto);
         return ResponseEntity.ok(responseDto);
     }
@@ -35,13 +35,13 @@ public class AccountController {
     }
 
     @PostMapping("/forgot-password")
-    public ResponseEntity<Void> forgotPassword(@RequestBody ForgotPwRequestDto requestDto) {
+    public ResponseEntity<Void> forgotPassword(@RequestBody @Valid ForgotPwRequestDto requestDto) {
         accountService.forgotPassword(requestDto);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/update-password")
-    public ResponseEntity<Void> updatePassword(HttpServletRequest request, @RequestBody ChangePwRequestDto requestDto) {
+    public ResponseEntity<Void> updatePassword(HttpServletRequest request, @RequestBody @Valid ChangePwRequestDto requestDto) {
         accountService.updatePassword(request, requestDto);
         return ResponseEntity.ok().build();
     }
@@ -52,8 +52,8 @@ public class AccountController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/change-nickname")
-    public ResponseEntity<Void> changeNickname(HttpServletRequest request, @RequestBody ChangeNicknameRequestDto requestDto) {
+    @PatchMapping("/change-nickname")
+    public ResponseEntity<Void> changeNickname(HttpServletRequest request, @RequestBody @Valid ChangeNicknameRequestDto requestDto) {
         accountService.changeNickname(request, requestDto);
         return ResponseEntity.ok().build();
     }
--- src/main/java/org/likelion/zagabi/Domain/Account/Dto/Request/ChangeNicknameRequestDto.java
+++ src/main/java/org/likelion/zagabi/Domain/Account/Dto/Request/ChangeNicknameRequestDto.java
@@ -1,4 +1,12 @@
 package org.likelion.zagabi.Domain.Account.Dto.Request;
 
-public record ChangeNicknameRequestDto(String newNickName) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangeNicknameRequestDto(
+        @Size(max = 10, message = "닉네임은 최대 10자까지 입력 가능합니다.")
+        @Schema(description = "name", example = "멋쟁이슴우")
+        @NotBlank(message = "[ERROR] 닉네임 입력은 필수 입니다.")
+        String newNickName) {
 }
