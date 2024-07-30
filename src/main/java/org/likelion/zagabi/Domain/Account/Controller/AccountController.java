02c6c4315bb4b3289574ebf0dbacc11da4630db8
Geunsik Kim <geunsikevin@naver.com>
2024-07-30 15:01:01 +0900

    ✨feat: 인증 인가 최종 구현 완료(#7)

---
+++ src/main/java/org/likelion/zagabi/Domain/Account/Controller/AccountController.java
@@ -0,0 +1,60 @@
+package org.likelion.zagabi.Domain.Account.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.likelion.zagabi.Domain.Account.Dto.Request.*;
+import org.likelion.zagabi.Domain.Account.Dto.Response.UserLoginResponseDto;
+import org.likelion.zagabi.Domain.Account.Dto.Response.UserSignUpResponseDto;
+import org.likelion.zagabi.Domain.Account.Entity.User;
+import org.likelion.zagabi.Domain.Account.Service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
+        UserLoginResponseDto responseDto = accountService.login(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserSignUpResponseDto> signup(@RequestBody UserSignUpRequestDto requestDto) {
+        UserSignUpResponseDto responseDto = accountService.signup(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        accountService.logout(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<Void> forgotPassword(@RequestBody ForgotPwRequestDto requestDto) {
+        accountService.forgotPassword(requestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<Void> updatePassword(HttpServletRequest request, @RequestBody ChangePwRequestDto requestDto) {
+        accountService.updatePassword(request, requestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete-account")
+    public ResponseEntity<Void> deleteAccount(HttpServletRequest request) {
+        accountService.deleteAccount(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/change-nickname")
+    public ResponseEntity<Void> changeNickname(HttpServletRequest request, @RequestBody ChangeNicknameRequestDto requestDto) {
+        accountService.changeNickname(request, requestDto);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/org/likelion/zagabi/Domain/Account/Dto/Request/ChangeNicknameRequestDto.java
@@ -0,0 +1,4 @@
+package org.likelion.zagabi.Domain.Account.Dto.Request;
+
+public record ChangeNicknameRequestDto(String newNickName) {
+}
