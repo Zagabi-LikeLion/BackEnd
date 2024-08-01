1f633c6098ec84d86340b20643ff7eaaa4ce9fa9
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-08-01 18:07:13 +0900

    ✨  feat : 스웨거 설명 추가 및 가치관 휴지통 수정 (#52)

--- src/main/java/org/likelion/zagabi/Domain/ValueTrashCan/Controller/ValueTrashCanController.java
+++ src/main/java/org/likelion/zagabi/Domain/ValueTrashCan/Controller/ValueTrashCanController.java
@@ -1,5 +1,7 @@
 package org.likelion.zagabi.Domain.ValueTrashCan.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.likelion.zagabi.Domain.ValueTrashCan.Dto.request.CreateValueTrashCanRequestDto;
@@ -17,21 +19,25 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/valuetrashcan")
+@Tag(name = "가치관 휴지통 API", description = "가치관 휴지통 API 입니다.")
 public class ValueTrashCanController {
     private final ValueTrashCanService valueTrashCanService;
     private final ValueTrashCanQueryService valueTrashCanQueryService;
 
+    @Operation(method = "POST", summary = "가치관 휴지통 생성", description = "가치관 휴지통 생성을 합니다. header에 accessToken과 body에는 가치관 ID를 담아서 전송합니다.")
     @PostMapping("")
     public ResponseEntity<?> createValueTrashCan(@AuthenticationPrincipal UserDetails userDetails, @RequestBody CreateValueTrashCanRequestDto createValueTrashCanRequestDto){
         ValueTrashCanResponseDto valueTrashCanResponseDto = valueTrashCanService.createValueTrashCan(userDetails.getUsername(), createValueTrashCanRequestDto);
         return new ResponseEntity<>(valueTrashCanResponseDto, HttpStatus.CREATED);
     }
 
+    @Operation(method = "GET", summary = "가치관 휴지통 조회", description = "가치관 휴지통 조회를 합니다. header에 accessToken과 url parameter에는 가치관 변화 기록 ID를 담아서 전송합니다.")
     @GetMapping("/{valueTrashCanId}")
     public ResponseEntity<?> getValueTrashCan(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Long valueTrashCanId){
         return ResponseEntity.ok(valueTrashCanQueryService.getValueTrashCan(valueTrashCanId));
     }
 
+    @Operation(method = "GET", summary = "가치관 휴지통 전체 조회", description = "가치관 휴지통 전체 조회를 합니다. header에 accessToken을 담아서 전송합니다.")
     @GetMapping("/getAll")
     public ResponseEntity<List<ValueTrashCanResponseDto>> getAllValueTrashCan(@AuthenticationPrincipal UserDetails userDetails) {
         List<ValueTrashCanResponseDto> valueTrashCanResponseDtos = valueTrashCanQueryService.getAllValueTrashCan(userDetails.getUsername());
